4f7319a1ef1aa7942593a3be2e08144333c8d03a
Zack Siri <zack@artellectual.com>
2014-05-26 16:58:17 +0700

    added middleman bin

--- config.rb
+++ config.rb
@@ -53,9 +53,6 @@ set :images_dir, 'images'
 
 activate :directory_indexes
 
-activate :relative_assets
-set :relative_links, true
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
